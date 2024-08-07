683fbdfac06b57d0926caa0d999de8419d11bf76
Martin Njoroge <martinn@innova.co.ke>
2024-08-07 23:42:25 +0300

    Add Url paths for FXOpen

---
+++ FXOpenAPI.Logic/StaticStrings/Url.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace FXOpenAPI.Logic.StaticStrings
+{
+    public static class Url
+    {
+        public const string BaseUrl = "https://marginalttdemowebapi.fxopen.net/api/v2/";
+        
+        public const string Account = "account";
+        public const string Currency = "currency";
+        public const string CurrencyType = "currency/type";
+        public const string QuoteHistory = "quotehistory";
+        public const string Version = "version";
+        public const string Symbols = "symbols";
+        public const string Symbol = "symbol";
+        public const string BarsAsk = "bars/ask";
+        public const string BarsBid = "bars/bid";
+        public const string Info = "info";
+        public const string Count = "count";
+        public const string Timestamp = "timestamp";
+        public const string Periodicities = "periodicities";
+    }
+}
